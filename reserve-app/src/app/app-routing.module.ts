adce42d624a50eb1c9d2c71984047524f904add4
takuya <takuan.tt.tkhs.09@gmail.com>
2021-01-04 23:58:54 +0900

    フロント一旦完成

--- reserve-app/src/app/app-routing.module.ts
+++ reserve-app/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductRoutingModule } from './product/product-routing.module';
 import { ProductModule } from './product/product.module';
 
 const routes: Routes = [
@@ -10,8 +9,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    // ルーティングモジュール読み込み
-    ProductRoutingModule
+    ProductModule
   ],
   exports: [RouterModule]
 })
--- reserve-app/src/app/product/product.module.ts
+++ reserve-app/src/app/product/product.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductComponent } from './product.component';
 
-
+const routes: Routes = [
+  { path: '', component: ProductListingComponent  },
+  { path: ':productId', component: ProductDetailComponent  },
+];
 
 @NgModule({
   declarations: [
     // コンポーネント宣言
     ProductListingComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductComponent
   ],
   imports: [
     // *ngForなど利用するため
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ProductModule { }
