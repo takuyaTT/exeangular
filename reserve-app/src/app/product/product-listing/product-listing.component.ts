6b831e328bcf67974af351e61842cfe9a547e906
takuya <takuan.tt.tkhs.09@gmail.com>
2021-01-09 18:41:44 +0900

    フロント＆バックエンド結合

--- reserve-app/src/app/product/product-listing/product-listing.component.ts
+++ reserve-app/src/app/product/product-listing/product-listing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../product';
+import { Observable } from 'rxjs';
+import { ProductService } from '../shared/product.service';
+
 
 @Component({
   selector: 'app-product-listing',
@@ -7,11 +9,47 @@ import { products } from '../product';
   styleUrls: ['./product-listing.component.scss']
 })
 export class ProductListingComponent implements OnInit {
-  products = products;
+  products :any
   
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.products = this.productService.getProducts();    
+
+    const productObservable = this.productService.getProducts();
+
+    productObservable.subscribe(
+      (data)=>{
+        this.products = data;
+      },
+      (err)=>{
+        console.log("ERROR")
+      }
+      // completeは不要
+      //()=>{
+        //console.log("COMP")
+      //}
+    );
   }
+    /*
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+      console.log('just before subscribe');
+      observable.subscribe({
+        next(x) { console.log('got value ' + x); },
+        error(err) { console.error('something wrong occurred: ' + err); },
+        complete() { console.log('done'); }
+      });
+      console.log('just after subscribe');
+  }
+  */
 
 }
