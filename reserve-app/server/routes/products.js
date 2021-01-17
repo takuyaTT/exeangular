dece252c10808485d8c2d95f8b8ebda4f15713aa
takuya <takuan.tt.tkhs.09@gmail.com>
2021-01-17 19:54:17 +0900

    認証関連の追加

--- reserve-app/server/routes/products.js
+++ reserve-app/server/routes/products.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const Product = require('../model/product');
 const router = express.Router();
+const UserCtrl = require('../controllers/user');
+
+router.post('/secret',UserCtrl.authMiddleware, (req,res)=>{
+  return res.json({"sercret": true});
+});
 
 router.get('',(req,res)=>{
   Product.find({},(err,foundProducts)=>{
@@ -8,7 +13,7 @@ router.get('',(req,res)=>{
   })
 });
 
-router.get('/:productId',(req,res)=>{
+router.get('/:productId', UserCtrl.authMiddleware, (req,res)=>{
   const productId = req.params.productId;
   Product.findById(productId,(err,foundProduct)=>{
     if(err){
--- reserve-app/src/app/auth/auth.module.ts
+++ reserve-app/src/app/auth/auth.module.ts
@@ -6,6 +6,9 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthService } from './shared/auth.service';
 import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './shared/auth.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './shared/token.interceptor';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -25,7 +28,13 @@ const routes: Routes = [
     FormsModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ],
   bootstrap:[
     
